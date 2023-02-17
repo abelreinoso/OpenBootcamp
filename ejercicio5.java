d1f6972fd99ce98ffd050e87d267293532856f3a
abelreinoso <111579680+abelreinoso@users.noreply.github.com>
2023-02-17 11:03:57 -0300

    Add files via upload

---
+++ ejercicio5.java
@@ -0,0 +1,37 @@
+package com.openbootcamp.Ejercitacion.ejerciciosTema5;
+
+public class ejercicio5 {
+
+    public interface CocheCRUD{
+         void save();
+         void findAll();
+         void delete();
+    }
+
+    public static class CocheCRUDImpl implements CocheCRUD {
+        @Override
+        public void save() {
+            System.out.println("Save = Coche guardado");
+        }
+        @Override
+        public void findAll() {
+            System.out.println("findAll = Coche encontrado");
+        }
+        @Override
+        public void delete() {
+            System.out.println("delete = Coche borrado");
+        }
+        @Override
+        public String toString() {
+            return "CocheCRUDImpl{}";
+        }
+    }
+
+    public static void main(String[] args) {
+        CocheCRUD coche = new CocheCRUDImpl();
+        coche.save();
+        coche.findAll();
+        coche.delete();
+    }
+
+}
